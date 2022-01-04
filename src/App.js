3510ae959df36431b0041bc642534e6760dcce75
BFlorian91 <b.florian91@icloud.com>
2022-01-04 22:36:50 +0100

    fetching tutorial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        "https://covid19-api.com/country/all?format=json"
+      );
+      const data = await res.json();
+      setData(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) return <div className="loading"><ClipLoader color={'#FFF'} size={'50px'} /></div>;
+  if (error) return <div className="error">❌ {error}</div>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">🌿 All Countries 🌴</h1>
+        {data.map((country, index) => (
+          <p className="country" key={index}>
+            {country.country} <br />
+            Deaths: {country.deaths}
+          </p>
+        ))}
       </header>
     </div>
   );
